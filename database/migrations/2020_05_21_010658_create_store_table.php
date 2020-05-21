c5b29cbbc47332b09d2a444e9aba08af7cb50aca
Lucas Valente <lucas.valente9999@gmail.com>
2020-05-20 22:27:00 -0300

    Iniciado testes.
    Criado a migration para a tabela store, o model, um factory básico e uma seed.

---
+++ database/migrations/2020_05_21_010658_create_store_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('description');
+            $table->string('phone');
+            $table->string('mobile_phone');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
